b8c70b64f7ff435d9b2538aea8fdcbf175d53a58
talma <tal.mandelovitz@mail.huji.ac.il>
2021-07-29 15:42:59 +0300

    activities

---
+++ app/src/main/java/huji/postpc/y2021/talme/myapplication/Requests.java
@@ -0,0 +1,14 @@
+package huji.postpc.y2021.talme.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Requestes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requestes);
+    }
+}
---
+++ app/src/main/java/huji/postpc/y2021/talme/myapplication/Welcome.java
@@ -0,0 +1,14 @@
+package huji.postpc.y2021.talme.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
