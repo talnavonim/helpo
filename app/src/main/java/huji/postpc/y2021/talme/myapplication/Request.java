62013fd38c994a6cf8d90e45dc8b4abca5418cf1
talma <tal.mandelovitz@mail.huji.ac.il>
2021-07-29 17:56:18 +0300

    activities2

--- app/src/main/java/huji/postpc/y2021/talme/myapplication/Request.java
+++ app/src/main/java/huji/postpc/y2021/talme/myapplication/Request.java
@@ -1,4 +1,5 @@
 package huji.postpc.y2021.talme.myapplication;
 
 public class Request {
+    //needs to have timeout
 }
--- app/src/main/java/huji/postpc/y2021/talme/myapplication/RequestsActivity.java
+++ app/src/main/java/huji/postpc/y2021/talme/myapplication/RequestsActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Requests extends AppCompatActivity {
+public class RequestsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
